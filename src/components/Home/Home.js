73d00f50aac19c6750fa1fbd5920dfb11b605103
vanshgoswami000 <vanshofficial00@gmail.com>
2026-05-01 23:33:26 +0530

    fix unused github import

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
-import Github from "../About/Github";
+
 import {
   AiFillGithub,
   AiFillInstagram,
